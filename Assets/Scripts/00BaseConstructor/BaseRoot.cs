829a49e32a033bb03b5f546fbd6c8d5cfa686aa5
SangonomiyaSakunovi <1602274566@qq.com>
2023-10-02 12:07:43 +0800

    Update BaseRoot.cs

--- Assets/Scripts/00BaseConstructor/BaseRoot.cs
+++ Assets/Scripts/00BaseConstructor/BaseRoot.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseWindow : MonoBehaviour
 {
-    
+    protected BaseService _service = null;
+
+    public void SetWindowState(bool isActive = true)
+    {
+        if (gameObject.activeSelf != isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+        if (isActive)
+        {
+            InitWindow();
+        }
+        else
+        {
+            ClearWindow();
+        }
+    }
+
+    protected virtual void InitWindow()
+    {
+
+    }
+
+    protected virtual void ClearWindow()
+    {
+
+    }
+
+    protected T GetOrAddComponent<T>(GameObject gameObject) where T : Component
+    {
+        T t = gameObject.GetComponent<T>();
+        if (t == null)
+        {
+            t = gameObject.AddComponent<T>();
+        }
+        return t;
+    }
+
+    protected void RemoveComponent<T>(GameObject gameObject) where T : Component
+    {
+        T t = gameObject.GetComponent<T>();
+        if (t != null)
+        {
+            Destroy(t);
+        }
+    }
 }
 
-public class BaseService: MonoBehaviour
+public class BaseService : MonoBehaviour
 {
 
 }
 
-public class BaseSystem: MonoBehaviour
+public class BaseSystem : MonoBehaviour
 {
 
 }
