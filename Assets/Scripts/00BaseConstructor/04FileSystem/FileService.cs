59e43eb71ae4bf15a373e01a0163fff04a19ddf3
SangonomiyaSakunovi <1602274566@qq.com>
2023-10-22 15:45:06 +0800

    update screenshot

--- Assets/Scripts/00BaseConstructor/04FileSystem/FileService.cs
+++ Assets/Scripts/00BaseConstructor/04FileSystem/FileService.cs
@@ -1,32 +1,20 @@
 ﻿using System;
-using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class FileService : BaseService<FileService>
 {
     private string _screenshotName;
-    private string _screenshotPath;
-    private string _desktopPath;
-
-    private void Start()
-    {
-        _desktopPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-    }
-
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.F12))
-    //    {
-    //        CaptureScreenshot();
-    //    }
-    //}
 
     public void CaptureScreenshot()
     {
         string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
         _screenshotName = "Screenshot_" + timestamp + ".png";
-        _screenshotPath = Path.Combine(_desktopPath, _screenshotName);
-        ScreenCapture.CaptureScreenshot(_screenshotPath);
-        Debug.Log("截图已保存到桌面：" + _screenshotPath);
+        string filePath = EditorUtility.SaveFilePanel("生成作品", "", _screenshotName, "png");
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            ScreenCapture.CaptureScreenshot(filePath);
+            Debug.Log("截图已保存到桌面：" + filePath);
+        }
     }
 }
