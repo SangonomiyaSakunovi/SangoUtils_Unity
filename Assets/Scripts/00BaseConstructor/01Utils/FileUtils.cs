bd583da9238d2f9eb85fc2c2e2fe1e3b714a547f
SangonomiyaSakunovi <1602274566@qq.com>
2023-11-14 19:54:38 +0800

    update loadTexture runtime

--- Assets/Scripts/00BaseConstructor/01Utils/FileUtils.cs
+++ Assets/Scripts/00BaseConstructor/01Utils/FileUtils.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
+using UnityEngine;
 
-public class JsonUtils
+public static class JsonUtils
 {
     public static T LoadJsonFile<T>(string filePath)
     {
@@ -22,5 +24,49 @@ public class JsonUtils
     public static T DeJsonString<T>(string str)
     {
         return JsonSerializer.Deserialize<T>(str);
+    }
+}
+
+public static class TextureUtils
+{
+    private static string _texture2DType = "*.PNG|*.JPG";
+
+    public static List<Texture> LoadTextureFolder(string folderPath, int width, int length)
+    {       
+        if (!Directory.Exists(folderPath))
+        {
+            return null;
+        }
+        List<string> texturePaths = new List<string>();
+        string[] textureTypes = _texture2DType.Split('|');
+        for (int i = 0; i < textureTypes.Length; i++)
+        {
+            string[] textureDirs = Directory.GetFiles(folderPath, textureTypes[i]);
+            for (int j = 0; j < textureDirs.Length; j++)
+            {
+                texturePaths.Add(textureDirs[j]);
+            }
+        }
+        List<Texture> textureResults = new List<Texture>();
+        for (int k = 0; k < texturePaths.Count; k++)
+        {
+            Texture2D texture = new Texture2D(width, length);
+            byte[] textureByte = FileStreamUtils.GetBytes(texturePaths[k]);
+            texture.LoadImage(textureByte);
+            textureResults.Add(texture);
+        }
+        return textureResults;
+    }
+}
+
+public static class FileStreamUtils
+{
+    public static byte[] GetBytes(string filePath)
+    {
+        FileStream fileStream = new FileStream(filePath, FileMode.Open);
+        byte[] bytes = new byte[fileStream.Length];
+        fileStream.Read(bytes, 0, bytes.Length);
+        fileStream.Close();
+        return bytes;
     }
 }
